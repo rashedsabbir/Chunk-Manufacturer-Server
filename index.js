6e0f897295b44a6e35dcb4526da0ea19361d6489
rashedsabbir <rashedulhasan1996@gmail.com>
2022-05-26 02:05:21 +0600

    review added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const cors=require("cors")
-const {MongoClient}=require("mongodb")
+const {MongoClient, ServerApiVersion}=require("mongodb")
 const ObjectId=require("mongodb").ObjectId
 require("dotenv").config();
 
@@ -10,6 +10,7 @@ const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 
+//server running
 app.get("/",(req,res)=>{
     res.send("Successfully Run The Node And Express")
 })
@@ -17,16 +18,38 @@ app.get("/check",(req,res)=>{
     res.send("Check  this for remote url change")
 })
 
+//MongoDB connect
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.57lxm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
+
 async function run() {
     try {
+        await client.connect();
+        const database = client.db("Chunk-Manufacturer");
+        const reviews = database.collection("reviews");
 
-        
+
+        app.get("/reviews",async(req,res)=>{
+            const result=await reviews.find({}).toArray()
+            res.json(result)
+          })
+          app.post("/reviews",async(req,res)=>{
+            const item=req.body
+            const result=await reviews.insertOne(item)
+            res.json(result)
+          })
+
+          
     } finally {
         //   await client.close();
         }
     }
 run().catch(console.dir);
 
+//listening to port 5000
 app.listen(port,()=>{
     console.log("Running the Localhost",`http://localhost:${port}`)
 })
